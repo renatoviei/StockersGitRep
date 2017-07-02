fe55419c3dac83813f87224cf74b6f781b2dfab7
Brunosimoesorlandelli <brunosorlandelli@gmail.com>
2017-07-01 22:53:00 -0300

    adição da classe basica Pedido

---
+++ Stockers/src/Negocio/Beans/Pedido.java
@@ -0,0 +1,54 @@
+package Negocio.Beans;
+
+public class Pedido {
+	private Loja loja;
+	private Produto produto;
+	private int quantidade;
+	private int valor;
+	private String observacao;
+
+	public Pedido() {
+
+	}
+
+	public Loja getLoja() {
+		return loja;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+	public void setLoja(Loja loja) {
+		this.loja = loja;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+
+}
