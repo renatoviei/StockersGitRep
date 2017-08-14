68716617585f66b348d462840f31d26a9210d2f3
Mateus Fittipaldi <mateusfitti@gmail.com>
2017-08-14 17:43:56 -0300

    GRANT do banco
    Inserido

---
+++ Stockers/GRANT_Stockers.sql
@@ -0,0 +1,12 @@
+USE stockers;
+
+CREATE USER IF NOT EXISTS 'admSistema'@'localhost' IDENTIFIED BY 'senha123';
+CREATE USER IF NOT EXISTS 'lojaSistema'@'localhost' IDENTIFIED BY 'senha123';
+
+GRANT ALL ON stockers.* TO 'admSistema'@'localhost';
+
+GRANT SELECT ON stockers.produto TO 'lojaSistema'@'localhost';
+GRANT INSERT, UPDATE ON stockers.loja TO 'lojaSistema'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON stockers.pedidos TO 'lojaSistema'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON stockers.pagamento TO 'lojaSistema'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON stockers.item_produto TO 'lojaSistema'@'localhost';
